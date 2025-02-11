95ef48edbb32fdf1d1fa929b0b0270121bfbb449
NurD891 <nurdanabdyganiev58@gmail.com>
2025-02-11 12:21:05 +0600

    first change

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.nextLine();
+        scanner.close();
+
+        int sum = 0;
+        for (char c : n.toCharArray()) {
+            sum += c;
+        }
+
+        if (sum % 3 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
